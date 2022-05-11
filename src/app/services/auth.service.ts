04f7642630ebc38fdcfeeaae83f1c0c66e2c4960
Soraya Sanchez <sorayasanchez0503@gmail.com>
2022-05-11 09:49:29 -0500

    Firebase y servicios REST

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,31 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { UsuarioModel } from '../models/usuario.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private url = 'https://identitytoolkit.googleapis.com/v1/accounts';
+  private apikey = 'AIzaSyBGukhfUMv5GPFRekcs5Mo-Ygyd6GEog_0';
+
+//Crear nuevo usuario 
+ // https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=[API_KEY]
+
+ //login
+ //https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=[API_KEY]
+
+  constructor(private http: HttpClient ) { }
+
+  logout(){
+
+  }
+  login (usuario: UsuarioModel){
+
+  }
+
+  nuevoUsuario ( usuario: UsuarioModel){
+    
+  }
+}
+

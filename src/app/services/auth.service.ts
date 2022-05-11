0f65ac0617047f6b90bfc6aab83895079eff810c
Soraya Sanchez <sorayasanchez0503@gmail.com>
2022-05-11 18:51:40 -0500

    Registrar un nuevo usuario

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -25,7 +25,18 @@ export class AuthService {
   }
 
   nuevoUsuario ( usuario: UsuarioModel){
-    
+
+    const authData = {
+      email: usuario.email, 
+      password: usuario.password,
+      returnSecureToken: true
+
+    };
+    return this.http.post(
+      `${ this.url }:signUp?key=${this.apikey}`,
+      authData
+    );
+
   }
 }
 
